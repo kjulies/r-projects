4c097d33fbf285eb55ea975e23cfba5c7d86a34f
Samuel Gil <samuxiii@gmx.com>
2018-10-06 17:42:58 +0200

    Resolving a binomial distribution problem

---
+++ basic-3.r
@@ -0,0 +1,3 @@
+# Unfair coin and you flip 8 times. Probability heads is 0.4. What's the probability to have 2 heads?
+# 8 athletes to compete in Olympic Games. Probability of making the final race is 0.4. What's the probability that 2 athletes make it into the final race?
+dbinom(2,8,0.4)
