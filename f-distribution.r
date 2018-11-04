087b2b9103fc6bf3e844e22ae007aeaf7590f596
Samuel Gil <samuxiii@gmx.com>
2018-11-04 10:49:38 +0100

    Added comments and fixes

--- f-distribution.r
+++ f-distribution.r
@@ -1,19 +1,29 @@
-SSR_R <- 198.3
-SSR_U <- 183.2
+# Unrestricted: y = b_0 + b_1*x_1 + b_2*x_2 + b_3*x_3 + b_4*x_4
+# Restricted:   y = b_0 + b_3*x_3 + b_4*x_4
+#
+# Testing: 
+#
+# H_0:             b_1 = b_2 = 0   <= null hyphotesis
+# H_Alternative:   b_1 != 0 or b_2 != 0
+
+# We are given this data
+SSR_R <- 198.3 #sum squares residual for restricted
+SSR_U <- 183.2 #sum squares residual for unrestricted
 alpha <- 0.05
-r = 2
-n = 500
-k = 4
+r = 2 #restrictions (dropped coeficients)
+n = 500 #observations
+k = 4 #number of coeficients of regressors (b_0 or intercept is not counted here)
 
-#this test is one-sided hypotheses, so only one tail
-f_critical_value = qf(alpha, r, n-(k+1), lower.tail = FALSE)
-F_stat = ((SSR_R - SSR_U)/r)/(SSR_U/(n-(k+1)))
+# This test is one-sided hypotheses, so only one tail
+F_critical_value = qf(alpha, r, n-(k+1), lower.tail = FALSE)
+F_statistic = ((SSR_R - SSR_U)/r)/(SSR_U/(n-(k+1)))
 
-print(f_critical_value)
-print(F_stat)
+print(F_critical_value)
+print(F_statictic)
 
-if (F_stat>f_critical_value){
-  print("Reject de null")
+if (F_stat > F_critical_value){
+  print("Reject de null hyphotesis")
 }else{
-  print("No reject de null")
+  print("No reject de null hyphotesis")
 }
+
