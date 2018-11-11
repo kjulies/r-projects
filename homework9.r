77d5c74302f90d194d602aee86b1f2571deb7c95
Samuel Gil <samuxiii@gmx.com>
2018-11-11 15:31:40 +0100

    Difference in difference

--- homework9.r
+++ homework9.r
@@ -26,4 +26,15 @@ summary(model3)
 # avg starting wage (state=0) = 4.62863
 # avg starting wage (state=1) = b_0 + b_1*S = 4.62863 + (-0.02123)*1 = 4.60707
 # Can we reject the null hypothesis that the avg starting wage is the same in state=1 and state=0?
+#    To be the same, b_1 must be 0. So..
 #    No, we CAN'T because the p-value of null hypothesis (when b_1 = 0) is 0.638 (big for typical confidence levels)
+
+
+# taking into account pa1 y pa2 for full-time employment
+model4 = lm(formula = empft ~ state + pa1 + pa2, data = fastfood)
+summary(model4) #error, not estimable because state, pa1 and pa2 are collinear
+
+# difference in difference
+model5 = lm(formula = empft2 - empft ~ state , data = fastfood)
+summary(model5) # DiD = 3.443
+
